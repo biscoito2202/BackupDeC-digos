05d09f3db43edd16c3cd91f7c7b63248cd089895
biscoito2202 <evelyn.lorkievicz@gmail.com>
2024-10-13 21:02:56 -0300

    Array01

---
+++ Array01/script.js
@@ -0,0 +1,17 @@
+// VARIAVEIS UNITARIA
+const name = 'Bianca'
+const nome2 = 'Julia'
+const nome3 = 'Maria'
+// ...
+
+
+
+
+// VARIAVEIS DE ARRAY
+const nomes = ['Bianca', 'Julia', 'Maria']
+
+// IMPRIMINDO OS VALORES DO ARRAY
+console.log(nomes)
+console.log(nomes[2])
+nomes[10] = "Dino" // CRIANDO DINAMICAMENTE
+console.log(nomes[10])
